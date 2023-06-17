e09ff02915fb6c23bd03fd89345d27f3b4bda07c
Koschei <nitianzero@gmail.com>
2023-06-17 14:30:11 +0800

    feat: implement rm_scan

--- src/record/rm_scan.cpp
+++ src/record/rm_scan.cpp
@@ -18,7 +18,11 @@ See the Mulan PSL v2 for more details. */
 RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
     // Todo:
     // 初始化file_handle和rid（指向第一个存放了记录的位置）
-
+    rid_.page_no = RM_FIRST_RECORD_PAGE;
+    // 设置 -1 Bit::next_bit 即是 0
+    // 直接设置为 0，会少判断 0
+    rid_.slot_no = -1;
+    next();
 }
 
 /**
@@ -28,6 +32,16 @@ void RmScan::next() {
     // Todo:
     // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
 
+    while (rid_.page_no < file_handle_->file_hdr_.num_pages) {
+        auto pageHander = file_handle_->fetch_page_handle(rid_.page_no);
+        // 这里是 record 的数量不是 bitSize
+        rid_.slot_no = Bitmap::next_bit(true, pageHander.bitmap, file_handle_->file_hdr_.num_records_per_page, rid_.slot_no);
+        if (rid_.slot_no < file_handle_->file_hdr_.num_records_per_page) {
+            return;
+        }
+        rid_.page_no++;
+        rid_.slot_no = -1;
+    }
 }
 
 /**
@@ -35,8 +49,7 @@ void RmScan::next() {
  */
 bool RmScan::is_end() const {
     // Todo: 修改返回值
-
-    return false;
+    return rid_.page_no >= file_handle_->file_hdr_.num_pages;
 }
 
 /**
