9b79841fed82eb73ef5722c8ca4e5dc3dab83750
Koschei <nitianzero@gmail.com>
2023-07-20 07:51:41 +0800

    fix: do not loop read and write on page_table_

--- src/storage/buffer_pool_manager.cpp
+++ src/storage/buffer_pool_manager.cpp
@@ -262,17 +262,25 @@ void BufferPoolManager::delete_all_pages(int fd) {
 
     std::scoped_lock lock{latch_};
 
+    std::vector<PageId> pagesId;
+
+    // 不能一边读一边对遍历对象做写操作
     for (auto& entry : page_table_) {
         PageId pageId = entry.first;
         if (pageId.fd == fd && pageId.page_no != INVALID_PAGE_ID) {
-            frame_id_t frameId = entry.second;
-            // 只有当前页面不被使用才能删除
-            assert(pages_[frameId].pin_count_ == 0);
-            disk_manager_->write_page(pageId.fd, pageId.page_no, pages_[frameId].get_data(), PAGE_SIZE);
-            page_table_.erase(pageId);
-            pages_[frameId].reset_memory();
-            pages_[frameId].is_dirty_ = false;
-            free_list_.push_back(frameId);
+            pagesId.emplace_back(pageId);
         }
+    }
+    for (auto& pageId : pagesId) {
+        frame_id_t frameId = page_table_.at(pageId);
+        // 只有当前页面不被使用才能删除
+        // replacer_->unpin(frameId);
+        assert(pages_[frameId].pin_count_ == 0);
+        // 文件close了写不了
+        // disk_manager_->write_page(pageId.fd, pageId.page_no, pages_[frameId].get_data(), PAGE_SIZE);
+        page_table_.erase(pageId);
+        pages_[frameId].reset_memory();
+        pages_[frameId].is_dirty_ = false;
+        free_list_.push_back(frameId);
     }
 }
